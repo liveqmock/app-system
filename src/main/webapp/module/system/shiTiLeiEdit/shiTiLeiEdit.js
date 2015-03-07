49af3e13d5694b75a116b958afffbf300ae0a251
Administrator <Administrator@wangyang>
2015-03-07 19:39:23 +0800

    自动同步实体类定义，拆分不同系统的实体类mapping

--- src/main/webapp/module/system/shiTiLeiEdit/shiTiLeiEdit.js
+++ src/main/webapp/module/system/shiTiLeiEdit/shiTiLeiEdit.js
@@ -1,5 +1,62 @@
+function onPageRady(){
+  $('#newZiDuanBtn').click(function(){
+  		//增加新的字段
+  		var stlRecord = LUI.Datasource.getInstance('shiTiLeiRecord').getRecord(0);
+    	var zdsRecordset = stlRecord.getFieldValue('zds');
+    	zdsRecordset.addRecord({});
+  });
+  	
+}
 
-function onShiTiLeiSubmit(){
-	var r = LUI.Datasource.getInstance('shiTiLeiRecord').getRecord(0);
-  	var pkValue = r.as; 
+function onShiTiLeiSubmit(ds,ob,evt){
+    if(evt.params.success){
+      	//提示是否生成实体类定义文件
+      	var r = ds.getRecord(0);
+      	var shiTiLeiMC = r.getFieldValue('shiTiLeiMC'); 
+      	var shiTiLeiDM = r.getFieldValue('shiTiLeiDM');
+      
+      LUI.Message.confirm('保存成功...','是否生成新版本的实体类定义文件！',function(success){
+        	if(success){
+        		$.ajax({
+                  url: "http://"+_urlInfo.host+":"+_urlInfo.port+"/jservice/", 
+                  type: "POST", 
+                  data:{
+                      component:'nim-data',
+                      service:'data',
+                      method:'execute',
+                      arguments:"{" +
+                          "xiTongDH:'system'," +
+                          "gongNengDH:'stl'," +
+                          "caoZuoDH:'entityGenerate'," +
+                    	  "submitData:[{shiTiLeiDM:"+shiTiLeiDM+"}]" +
+                      "}"
+                  },
+                  dataType:"json",
+                  success: function(result){
+                      if(result.success){
+                          LUI.Message.info("信息","实体类定义文件生成成功！");
+                      }else{
+                          LUI.Message.info("信息",result.errorMsg);
+                      }
+                  },
+                  error:function(){
+                      LUI.Message.info("信息","访问服务器失败!");
+                  }
+              });
+        	}
+      });
+    }
+}
+
+function onGridRowRender(grid,ob,evt){
+  	var isInitial = evt.params.isInitial;
+    if(isInitial){
+		var rowEl = evt.params.rowEl;
+        var record = evt.params.record;
+        var rBtn = rowEl.find('#removeZiDuan');
+        rBtn.click(function(){
+            //删除当前记录
+            record.remove();
+        })
+    }
 }
--- src/main/webapp/module/system/shiTiLeiList/shiTiLeiList.js
+++ src/main/webapp/module/system/shiTiLeiList/shiTiLeiList.js
@@ -9,21 +9,67 @@ function onPageRady(){
 	$("#GFZRPS_zhengShi #GFZRPS_searchbtn").click(function(){
 		//
 		var gongYSMCValue = $('#GFZRPS_zhengShi').find('#gongYSMC:eq(0)').val();
-		if(gongYSMCValue==null || gongYSMCValue.length==0 || gongYSMCValue == "供方名称或供货范围"){
-			LUI.Grid.getInstance('cmp_displayGrid_zsgf').datasource.load({
+		if(gongYSMCValue==null || gongYSMCValue.length==0 || gongYSMCValue == "实体类代号/名称/表名"){
+			LUI.Grid.getInstance('shiTiLeiGrid').datasource.load({
 				filters : []
 			});
 		}else{
-			LUI.Grid.getInstance('cmp_displayGrid_zsgf').datasource.load({
+			LUI.Grid.getInstance('shiTiLeiGrid').datasource.load({
 				filters : [{
-					property : "gongYingSJBZL.danWei",
+					property : "shiTiLeiDH",
 					operator : 'like',
-                  	assist:"['gongHuoFW']",
+                  	assist:"['shiTiLeiMC','biaoMing']",
 					value : gongYSMCValue
 				}]
 			});
 		}
 	});
+  
+  	$('#appendBtn').click(function(){
+      	window.open("/nim.html?_pt_=system/shiTiLeiAppend/shiTiLeiAppend.html");
+    });
+  
+  	$('#generateBtn').click(function(){
+      	var submitData = [];
+  		var stlDataset = LUI.Datasource.getInstance('stlDataset');
+      	for(var i=0;i<stlDataset.size();i++){
+          	var stlRecord =  stlDataset.getRecord(i);
+          	submitData[submitData.length] = {
+              shiTiLeiDM:stlRecord.primaryFieldValue
+           	};
+      	}
+        LUI.Message.confirm('请确认...','是否对当前'+stlDataset.size()+'条实体类记录进行生成定义文件操作？',function(success){
+              if(success){
+                  $.ajax({
+                      url: "http://"+_urlInfo.host+":"+_urlInfo.port+"/jservice/", 
+                      type: "POST", 
+                      data:{
+                      component:'nim-data',
+                      service:'data',
+                      method:'execute',
+                      arguments:"{" +
+                      "xiTongDH:'system'," +
+                      "gongNengDH:'stl'," +
+                      "caoZuoDH:'entityGenerate'," +
+                      "submitData:"+LUI.Util.stringify(submitData) +
+                      "}"
+                      },
+                      dataType:"json",
+                      success: function(result){
+                    if(result.success){
+                      LUI.Message.info("信息","实体类定义文件生成成功！");
+                    }else{
+                      LUI.Message.info("信息",result.errorMsg);
+                    }
+                  },
+                    error:function(){
+                      LUI.Message.info("信息","访问服务器失败!");
+                    }
+                });
+             }
+        });
+  		
+  	});
 	
 }
 function onGridRowRender(grid,ob,event){
