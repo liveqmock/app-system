4f689e479ce05c4ce587a5561e86c8e7e845fd6f
Administrator <Administrator@wangyang>
2015-03-12 07:40:17 +0800

    功能、实体类的列表/编辑，及实体类的同步

--- src/main/webapp/module/system/shiTiLeiEdit/shiTiLeiEdit.js
+++ src/main/webapp/module/system/shiTiLeiEdit/shiTiLeiEdit.js
@@ -28,7 +28,7 @@ function onShiTiLeiSubmit(ds,ob,evt){
                           "xiTongDH:'system'," +
                           "gongNengDH:'stl'," +
                           "caoZuoDH:'entityGenerate'," +
-                    	  "submitData:[{shiTiLeiDM:"+shiTiLeiDM+"}]" +
+                    	"submitData:[{shiTiLeiDM:"+shiTiLeiDM+"}]" +
                       "}"
                   },
                   dataType:"json",
@@ -58,5 +58,106 @@ function onGridRowRender(grid,ob,evt){
             //删除当前记录
             record.remove();
         })
+    }
+}
+
+function beforeZiDuanOptionLoad(field,ob,evt){
+	var guanLianSTLField = field.cell.row.getCell('guanLianSTL').field;
+  	var guanLianSTLObj = guanLianSTLField.getValue();
+	if(guanLianSTLObj != null){
+	  	//重新设定下拉选项
+	  	field.datasource.addFilter('shiTiLei.id','=',guanLianSTLObj.shiTiLeiDM);
+	}else{
+		field.datasource.addFilter('shiTiLei.id','=',-1);
+	}
+}
+
+function ziDuanLXChanged(field,ob,evt){
+  	var newVal = evt.params.newValue;
+	console.info('ziDuanLXChanged');
+	var lieMingField = field.cell.row.getCell('lieMing').field;
+	var ziDuanCDField = field.cell.row.getCell('ziDuanCD').field;
+	var ziDuanJDField = field.cell.row.getCell('ziDuanJD').field;
+	var guanLianSTLField = field.cell.row.getCell('guanLianSTL').field;
+	var guanLianFLZDField = field.cell.row.getCell('guanLianFLZD').field;
+  
+    if(newVal==null){
+     	lieMingField.enable();
+     	ziDuanCDField.enable();
+     	ziDuanJDField.enable();
+     	guanLianSTLField.enable();
+     	guanLianFLZDField.enable();
+    }else{
+    	if(newVal.ziDuanLXDH == 'string'){//字符
+	    	if(ziDuanCDField.getValue()==null){
+	      		ziDuanCDField.setValue(newVal.ziDuanKD);
+	      	}
+			guanLianSTLField.setValue(null);
+	      	guanLianFLZDField.setValue(null);
+	
+	      	lieMingField.enable();
+	     	ziDuanCDField.enable();
+	     	ziDuanJDField.disable();
+	     	guanLianSTLField.disable();
+	     	guanLianFLZDField.disable();
+	    }else if(newVal.ziDuanLXDH == 'int'){//整数
+	      	if(ziDuanCDField.getValue()==null){
+	      		ziDuanCDField.setValue(newVal.ziDuanKD);
+	      	}
+			guanLianSTLField.setValue(null);
+	      	guanLianFLZDField.setValue(null);
+	
+	      	lieMingField.enable();
+	     	ziDuanCDField.enable();
+	     	ziDuanJDField.disable();
+	     	guanLianSTLField.disable();
+	     	guanLianFLZDField.disable();
+	    }else if(newVal.ziDuanLXDH == 'double'){//小数
+	      	if(ziDuanCDField.getValue()==null){
+	      		ziDuanCDField.setValue(newVal.ziDuanKD);
+	      	}
+	      	if(ziDuanJDField.getValue()==null){
+	      		ziDuanJDField.setValue(newVal.ziDuanJD);
+	      	}
+			guanLianSTLField.setValue(null);
+	      	guanLianFLZDField.setValue(null);
+	
+	      	lieMingField.enable();
+	     	ziDuanCDField.enable();
+	     	ziDuanJDField.enable();
+	     	guanLianSTLField.disable();
+	     	guanLianFLZDField.disable();
+	    }else if(newVal.ziDuanLXDH == 'date' || newVal.ziDuanLXDH == 'month'){//日期	 月份
+	      	ziDuanCDField.setValue(newVal.ziDuanKD);
+			guanLianSTLField.setValue(null);
+	      	guanLianFLZDField.setValue(null);
+	
+	      	lieMingField.enable();
+	     	ziDuanCDField.enable();
+	     	ziDuanJDField.disable();
+	     	guanLianSTLField.disable();
+	     	guanLianFLZDField.disable();
+	    }else if(newVal.ziDuanLXDH == 'text'){//文本
+	    	if(ziDuanCDField.getValue()==null){
+	      		ziDuanCDField.setValue(newVal.ziDuanKD);
+	      	}
+			guanLianSTLField.setValue(null);
+	      	guanLianFLZDField.setValue(null);
+	      	
+	      	lieMingField.enable();
+	     	ziDuanCDField.enable();
+	     	ziDuanJDField.disable();
+	     	guanLianSTLField.disable();
+	     	guanLianFLZDField.disable();
+	    }else if(newVal.ziDuanLXDH == 'set' || newVal.ziDuanLXDH == 'object'){//集合 对象	
+	      	ziDuanCDField.setValue(null);
+	      	ziDuanJDField.setValue(null);
+	
+	      	lieMingField.enable();
+	     	ziDuanCDField.disable();
+	     	ziDuanJDField.disable();
+	     	guanLianSTLField.enable();
+	     	guanLianFLZDField.enable();
+	    }
     }
 }
